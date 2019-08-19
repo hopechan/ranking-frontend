50a550e773b560f2e3d703d7de9873498212befe
Esperanza Duenas <da13002@ues.edu.sv>
2019-08-19 16:18:18 -0600

    tabla dentro de card, no tiene paginacion :v

--- src/components/TablaEstudiante/TablaEstudiante.jsx
+++ src/components/TablaEstudiante/TablaEstudiante.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Table } from 'reactstrap';
+import { Button, Card, CardTitle, CardBody,Table } from 'reactstrap';
 import API from "../server/api";
 
 class TablaEstudiante extends React.Component{
@@ -19,43 +19,49 @@ class TablaEstudiante extends React.Component{
     render() {
         return (
             <div>
-                <Table responsive>
-                    <thead>
-                        <tr>
-                            <th>Nombre</th>
-                            <th>Apellidos</th>
-                            <th>Fecha de Nacimiento</th>
-                            <th>Telefono</th>
-                            <th>email</th>
-                            <th>Direccion</th>
-                            <th>Año</th>
-                            <th>Seccion</th>
-                            <th>Centro Escolar</th>
-                            <th colspan="2">Opciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.estudiantes.map(e =>
-                            <tr>
-                                <td>{e.nombre}</td>
-                                <td>{e.apellidos}</td>
-                                <td>{e.fecha_nacimiento}</td>
-                                <td>{e.telefono}</td>
-                                <td>{e.email}</td>
-                                <td>{e.direccion}</td>
-                                <td>{e.anio}</td>
-                                <td>{e.seccion}</td>
-                                <td>{e.centro_escolar}</td>
-                                <td>
-                                    <Button color="warning">Editar</Button>
-                                </td>
-                                <td>
-                                    <Button color="danger">Borrar</Button>
-                                </td>
-                            </tr>
-                        )}
-                    </tbody>
-                </Table>
+                <div>
+                    <Card>
+                        <CardBody>
+                            <Table>
+                                <thead>
+                                    <tr>
+                                        <th>Nombre</th>
+                                        <th>Apellidos</th>
+                                        <th>Fecha de Nacimiento</th>
+                                        <th>Telefono</th>
+                                        <th>email</th>
+                                        <th>Direccion</th>
+                                        <th>Año</th>
+                                        <th>Seccion</th>
+                                        <th>Centro Escolar</th>
+                                        <th colspan="2">Opciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.state.estudiantes.map(e =>
+                                        <tr>
+                                            <td>{e.nombre}</td>
+                                            <td>{e.apellidos}</td>
+                                            <td>{e.fecha_nacimiento}</td>
+                                            <td>{e.telefono}</td>
+                                            <td>{e.email}</td>
+                                            <td>{e.direccion}</td>
+                                            <td>{e.anio}</td>
+                                            <td>{e.seccion}</td>
+                                            <td>{e.centro_escolar}</td>
+                                            <td>
+                                                <Button color="warning">Editar</Button>
+                                            </td>
+                                            <td>
+                                                <Button color="danger">Borrar</Button>
+                                            </td>
+                                        </tr>
+                                    )}
+                                </tbody>
+                            </Table>
+                        </CardBody>
+                    </Card>
+                </div>
             </div>
         )
     }
